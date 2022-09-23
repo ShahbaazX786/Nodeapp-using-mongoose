b92961a7d2729ea271baccda609b990e341363c3
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-23 17:37:25 +0530

    added a console msg which shows status on mongodb connection also a new collection,schema,model,document added.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // jshint esversion:6
 const mongoose = require('mongoose');
 
-const URL = "mongodb://localhost:27017/fruitsDB";
+// const URL = "mongodb://localhost:27017/fruitsDB";
 
-mongoose.connect(URL,{useNewUrlParser:true});
+mongoose.connect("mongodb://0.0.0.0:27017/fruitsDB",{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{//freaking sheet mongodb://0.0.0.0:27017/myapp somehow this sheet is working and mongodb://localhost:27017/ this sheet is not working in mongodb 6+.
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("connected to mongodb");
+    }
+}); 
 
 const fruitSchema = new mongoose.Schema({
     name:String,
@@ -21,3 +28,15 @@ const fruit =  new Fruit({
 
 fruit.save();
 
+const personSchema = mongoose.Schema({
+    name:String,
+    age:Number
+});
+
+const Person = mongoose.model("Person",personSchema);
+
+const person = new Person({
+    name:"Jack",
+    age:21
+});
+person.save();
