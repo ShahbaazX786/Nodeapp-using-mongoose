6b38bff1ec766ca712da9ab8ecffdf45aa9bd213
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-23 15:57:54 +0530

    added fruit schema

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const mongoose = require('mongoose');
 const URL = "mongodb://localhost:27017/fruitsDB";
 
 mongoose.connect(URL,{useNewUrlParser:true});
+
+const fruitSchema = new mongoose.Schema({
+    name:String,
+    rating:Number,
+    review:String
+});
